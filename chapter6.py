2068a862991636c31c90801e6c39f088056ee912
cantabile1129 <kanta.fukao@keio.jp>
2025-04-19 14:47:42 +0900

    code6.4.stage1まで

--- chapter6.py
+++ chapter6.py
@@ -75,67 +75,29 @@ out_feats = fe.duration_features(labels)
 
 print("入力特徴量のサイズ:", in_feats.shape)
 print("出力特徴量のサイズ:", out_feats.shape)
+
 # %%
 #code6.3
-
-import argparse
-#pythonインタプリタの動作制御（実行環境の状態や挙動を制御，コマンドライン引数を受け取る）
-import sys
-from concurrent.futures import ProcessPoolExecutor
-from pathlib import Path
-from tqdm import tqdm
-import numpy as np
-
-def get_parser():
-   parser = argparse.ArgumentParser(description="Preprocess duration models")
-   parser.add_argument("utt_list", type=str, help="utternace list")
-   parser.add_argument("lab_root", type=str, help="lab_root")
-   parser.add_argument("qst_file", type=str, help="HTS Style question file")
-   parser.add_argument("out_dir", type=str, help="output directory")
-   parser.add_argument("--n_jobs", type=int, default = 1, help="Number of jobs")
-   return parser
-
-def preprocess(lab_file, binary_dict, numeric_dict, in_Dir, out_dir):
-   labels=hts.loads(lab_file)
-   in_feats = fe.linguistic_features(labels, binary_dict, numeric_dict)
-   out_feats = fe.duration_features(labels)
-   utt_id = lab_file.stem
-   np.save(in_dir / f"{utt_id}-feats.npy", in_feats.astype(np.float32), allow_pickle =False)  
-   np.save(out_dir / f"{utt_id}-feats.npy", out_feats.astype(np.int32), allow_pickle =False)
-
-if __name__ == "__main__":
-   args = get_parser().parse_args(sys.argv[1:])
-   
-   with open(args.utt_list) as f :
-      utt_ids = [utt_id.strip() for utt_id in f]
-   lab_root = [Path(args.lab_root) / f"{utt_id}.lab" for utt_id in utt_ids]
-   binary_dict, numeric_dict = hts.load_question_set(args.qst_file)
-   in_dir = Path(args.out_dir) / "in_duration"
-   out_dir = Path(args.out_dir) / "out_duration"
-   in_dir.mkdir(parents=True, exist_ok=True)
-   out_dir.mkdir(parents=True, exist_ok=True)
-   
-   with ProcessPoolExecutor(args.n_jobs) as executor:
-      futures = [executor.submit(preprocess, lab_file, binary_dict, numeric_dict, in_dir, out_dir) for lab_file in lab_files]
-      for future in tqdm(futures):
-         future.result()
-# %%
-#code6.3.revised
-
+#Jupyterでargparseなしで引数を模倣
 from types import SimpleNamespace
 from pathlib import Path
+#プログレスバーの表示
 from tqdm import tqdm
 import numpy as np
 from nnmnkwii.io import hts
 from nnmnkwii.frontend import merlin as fe
+#マルチプロセス化
 from concurrent.futures import ProcessPoolExecutor
 
 # Jupyter上では argparse を使わずに引数を手動設定
 args = SimpleNamespace(
-    utt_list="data/utt_list.txt",
-    lab_root="data/lab",
-    qst_file="data/questions.hed",
-    out_dir="data/output",
+    utt_list="/home/takamichi-lab-pc05/ドキュメント/B4/Pythonで学ぶ音声合成/ttslearn/recipes/dnntts/data/utt_list.txt",
+    #code6.stage-1で作成
+    lab_root="/home/takamichi-lab-pc05/ドキュメント/B4/Pythonで学ぶ音声合成/ttslearn/recipes/dnntts/downloads/jsut-lab-0.1.1/basic5000/lab",
+    qst_file="/home/takamichi-lab-pc05/ドキュメント/B4/Pythonで学ぶ音声合成/ttslearn/recipes/common/qst1.hed",
+    #出力先ディレクトリ
+    out_dir="/home/takamichi-lab-pc05/ドキュメント/B4/Pythonで学ぶ音声合成/ttslearn/recipes/dnntts/data",
+    #並列処理で使うプロセス数
     n_jobs=2
 )
 
@@ -144,12 +106,16 @@ def preprocess(lab_file, binary_dict, numeric_dict, in_dir, out_dir):
     in_feats = fe.linguistic_features(labels, binary_dict, numeric_dict)
     out_feats = fe.duration_features(labels)
     utt_id = lab_file.stem
+    #in_duration/BASIC5000_0001-feats.npyのように入力
     np.save(in_dir / f"{utt_id}-feats.npy", in_feats.astype(np.float32), allow_pickle=False)
+    #out_duration/BASIC5000_0001-feats.npyのように出力
     np.save(out_dir / f"{utt_id}-feats.npy", out_feats.astype(np.int32), allow_pickle=False)
 
 # 処理の流れ
 with open(args.utt_list) as f:
+   #utt_list を読み込み、BASIC5000_0001 などのIDをリスト化
     utt_ids = [utt_id.strip() for utt_id in f]
+    #各IDから .lab ファイルのフルパスを作成
 lab_files = [Path(args.lab_root) / f"{utt_id}.lab" for utt_id in utt_ids]
 
 binary_dict, numeric_dict = hts.load_question_set(args.qst_file)
@@ -163,4 +129,58 @@ with ProcessPoolExecutor(args.n_jobs) as executor:
     futures = [executor.submit(preprocess, lab_file, binary_dict, numeric_dict, in_dir, out_dir) for lab_file in lab_files]
     for future in tqdm(futures):
         future.result()
+# %%
+#code6.4.stage1
+#コマンドラインではなくセルで実行できるように調整
+#notionの通りステージが-1から6まであり，順番に実行．
+import subprocess
+import os
+
+# 実行したいシェルスクリプトのコマンド
+command = ["./run.sh", "--stage", "1", "--stop-stage", "1"]
+#確実に絶対パスで指定
+working_dir = "/home/takamichi-lab-pc05/ドキュメント/B4/Pythonで学ぶ音声合成/ttslearn/recipes/dnntts"
+
+# 実行
+result = subprocess.run(command, cwd=working_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+# 標準出力と標準エラー出力の確認
+print("標準出力:")
+print(result.stdout)
+
+print("\n標準エラー:")
+print(result.stderr)
+# %%
+from ttslearn.dsp import world_spss_params
+
+#音響モデルの入力:言語特徴量
+in_feats = fe.linguistic_features(labels, binary_dict, numeric_dict, add_frame_features=True, subphone_features="corase_coding")
+
+#音声の読み込み
+_sr, x = wavfile.read(ttslearn.util.example_wav_file())
+sr = 16000
+x = (x / 32768).astype(np.float64)
+x = librosa.resample(x, _sr, sr)
+
+#音響モデルの出力:音響特徴量
+out_feats = world_spss_params(x, sr)
+
+#フレーム数の調整
+minL = min(in_feats.shape[0], out_feats.shape[0])
+in_feats = in_feats[:minL]
+out_feats = out_feats[:minL]
+
+#冒頭と末尾の非音声区間の長さを調整
+assert "sil" in labels.contexts[0] and "sil" in labels.context[-1]
+start_frame = int(labels.start_times[1]/50000)
+end_frame = int(labels.end_times[-2]/50000)
+
+#冒頭50ミリ秒，末尾100ミリ秒
+start_frame = max(0, start_frame - int(0.050/0.005))
+end_frame = min(minL, end_frame + int(0.100/0.005))
+
+in_feats = in_feats[start_frame:end_frame]
+out_feats = out_feats[start_frame:end_frame]
+
+
 # %%
