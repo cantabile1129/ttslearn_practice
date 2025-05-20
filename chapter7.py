7578e17194dc845981d45f410985af661eee34ed
cantabile1129 <kanta.fukao@keio.jp>
2025-05-20 12:53:55 +0900

    code7.4まで

--- chapter7.py
+++ chapter7.py
@@ -28,3 +28,40 @@ def inv_quantize(y, mu):
 def inv_mulaw_quantize(y, mu=255):
    return inv_mulaw(inv_quantize(y, mu), mu)
 
+#%%
+#code7.3
+import torch
+import torch.nn as nn
+class CausalConv1d(nn.Module):
+   def __init__(self, in_channels, out_channels, kernel_size, **kwargs):
+      super().__init__()
+      #入力長と出力長の差を解消するための片方分．
+      self.padding=kernel_size-1
+      #**kwargsは，キーワード付き引数（ex.)stride=1）をまとめて受け取り，dict形式で保持する構文．
+      #nn.Conv1dは，デフォルトでは左右対称のパディングを行う．
+      self.conv=nn.Conv1d(in_channels, out_channels, kernel_size, padding=self.padding, **kwargs)
+      
+   def forward(self, x):
+      y=self.conv(x)
+      #因果性を保つために，順方向シフトする．
+      if self.padding>0:
+         #yは[batch, channel, time]の形状を持つ3次元テンソル．
+         #右端のpadding分だけ未来の状態を持つので，それを削除する．
+         y=y[:, :, :-self.padding]
+      return y
+   
+#%%
+#code7.4
+
+class DilatedCausalConv1d(nn.Module):
+   def __init__(self, in_channels, out_channels, kernel_size, dilation=1, **kwargs):
+      super().__init__()
+      #入力の一番左に着目して，カーネルの一番右以外の個数をdilation factorだけかける．
+      self.padding=(kernel_size-1)*dilation
+      self.conv=nn.Conv1d(in_channels, out_channels, kernel_size, padding=self.padding, dilation=dilation, **kwargs)
+      
+   def forward(self, x):
+      y=self.conv(x)
+      if self.padding>0:
+         y=y[:, :, :-self.padding]
+      return y
